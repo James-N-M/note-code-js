907ec8417c803fc52dae85060c48d161587fe7f4
James Moore <jamesm0025@gmail.com>
2024-03-30 15:00:07 -0400

    vite bolierplate

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 // Add some interactivity to the website
+import { setupCounter } from './counter.js'
 console.log("Hello, world!");
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+        vitejs link
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+       javascript link
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
