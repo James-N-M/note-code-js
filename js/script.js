5c3f632d59fac8a0c8079e789cc6d2095251da94
James Moore <jamesm0025@gmail.com>
2024-03-31 10:11:09 -0400

    remove vite boilerplate

--- js/script.js
+++ js/script.js
@@ -1,44 +1,55 @@
-import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import * as monaco from "monaco-editor";
 
 self.MonacoEnvironment = {
-    getWorker(_, label) {
-        if(label === 'json') {
-            return new jsonWorker();
-        }
-        return new editorWorker();
+  getWorker: function (workerId, label) {
+    const getWorkerModule = (moduleUrl, label) => {
+      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+        name: label,
+        type: "module",
+      });
+    };
+
+    switch (label) {
+      case "json":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/json/json.worker?worker",
+          label
+        );
+      case "css":
+      case "scss":
+      case "less":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/css/css.worker?worker",
+          label
+        );
+      case "html":
+      case "handlebars":
+      case "razor":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/html/html.worker?worker",
+          label
+        );
+      case "typescript":
+      case "javascript":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/typescript/ts.worker?worker",
+          label
+        );
+      default:
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/editor/editor.worker?worker",
+          label
+        );
     }
-}
+  },
+};
 
-// Add some interactivity to the website
-// import { setupCounter } from './counter.js'
-// console.log("Hello, world!");
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//         vitejs link
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//        javascript link
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
-monaco.editor.create(document.getElementById('monaco-editor'), {
-    value: `function(x, y) { 
-  return x + y 
-}`,
-    language: 'javascript',
-    scrollbar: { vertical: 'hidden' },
-    theme: "vs-light",
+monaco.editor.create(document.getElementById("monaco-editor"), {
+  value: `function(x, y) { 
+        return x + y 
+    }`,
+  language: "javascript",
+  scrollbar: { vertical: "hidden" },
+  theme: "vs-light",
 });
+// setupCounter(document.querySelector('#counter'))
